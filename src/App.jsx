1e1fa8f1e90e3360624f88f4984ff2e49f3e89af
Kunal singh <ks6024108@gmail.com>
2025-01-24 20:52:05 +0530

    Home page created

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import HomePage from "./pages/HomePage";
+function App() {
+  return (
+    <React.Fragment>
+      <HomePage />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,51 @@
+import "../styles/HomePage.css";
+
+const HomePage = () => {
+  return (
+    <div className="home-container">
+      <header className="header">
+        <h1 className="title">Welcome to FarmAdvisor</h1>
+        <p className="subtitle">
+          Empowering Farmers with Smart Crop Recommendations
+        </p>
+      </header>
+
+      <div className="content">
+        <div className="image-container">
+          <img src="/cropHome.jpeg" alt="Farm field" className="farm-image" />
+        </div>
+        <div className="description">
+          <p>
+            FarmAdvisor helps farmers make informed decisions with accurate crop
+            recommendations and yield predictions tailored to their land.
+            Maximize your productivity with advanced data-driven insights.
+          </p>
+          <div className="links">
+            <a
+              href="https://crop-recommend-tb0f.onrender.com/"
+              className="link-button"
+              target="_blank"
+              // rel="noopener noreferrer"
+            >
+              Crop Recommendation
+            </a>
+            <a
+              href="https://crop-yield-prediction-per-country.onrender.com/"
+              className="link-button"
+              target="_blank"
+              // rel="noopener noreferrer"
+            >
+              Yield Prediction
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <footer className="footer">
+        <p>© 2025 FarmAdvisor. Cultivating Smarter Agriculture.</p>
+      </footer>
+    </div>
+  );
+};
+
+export default HomePage;
